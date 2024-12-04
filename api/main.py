715d2757b5114f4ee39a1989448dff09ed24b43e
ErrorNoName <ErrorNoName666@proton.me>
2024-12-04 13:00:20 +0100

    Add files via upload

--- api/main.py
+++ api/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import threading, random, os, requests, json
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 REGIONS = ['us-west', 'us-east', 'us-central', 'us-south', 'singapore', 'southafrica', 'sydney', 'rotterdam', 'brazil', 'hongkong', 'russia', 'japan', 'india', 'south-korea']
 
